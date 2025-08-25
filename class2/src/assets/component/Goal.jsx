025855adde7ddc9a7be8db29090fc864304bf509
itx-mj65 <jamshedweb65@gmail.com>
2025-08-25 15:53:35 +0500

    25 of august

--- class2/src/assets/component/Goal.jsx
+++ class2/src/assets/component/Goal.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import class2 from "../../media/pineapple.jpg"
 const Goal = () => {
+  const cars = ['Ford', 'BMW', 'Audi', "Mehran"];
   return (
-    <>
-    <h2>My Goal</h2>
-    <p>To learn React and build amazing applications!</p>
+    <>{
+      cars.map((car, index) => {
+        return (
+          <h1 key={index}  >{car}</h1>
+        )
+      })
+    }
+      <h2></h2>
+      <p>To learn React and build amazing applications!</p>
+      <img src={class2} height={200} width={300} />
+
+
     </>
   )
 }
