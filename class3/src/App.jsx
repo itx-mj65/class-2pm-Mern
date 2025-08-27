7147ee0cf33752a7c6eefaf3f1d3e3921aaa5ce7
itx-mj65 <jamshedweb65@gmail.com>
2025-08-27 15:37:16 +0500

    27 of august

---
+++ class3/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  console.log("reload")
+  const [count, setCount] = useState(false)
+  
+  return (
+    <>
+    {
+      count && <h1>hello world</h1>
+    }
+
+    <button  onClick={()=>setCount(!count) }  >Click me   
+    </button>
+    </>
+  )
+}
+
+export default App
