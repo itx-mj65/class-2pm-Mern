e5da02bef8554fc74dea6ce72d20eabdddc22d96
itx-mj65 <jamshedweb65@gmail.com>
2025-09-01 15:43:05 +0500

    1 of september

--- class3/src/App.jsx
+++ class3/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
   console.log("reload")
   const [count, setCount] = useState(false)
-  
+
+
+  useEffect(() => {
+    console.log("data is fetched")
+  })
   return (
     <>
-    {
-      count && <h1>hello world</h1>
-    }
+      {
+        count && <h1>hello world</h1>
+      }
 
-    <button  onClick={()=>setCount(!count) }  >Click me   
-    </button>
+      <button onClick={() => setCount(!count)}  >Click me
+      </button>
     </>
   )
 }
